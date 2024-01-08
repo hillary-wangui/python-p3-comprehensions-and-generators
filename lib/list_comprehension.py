6e7f4fd43db56a9dd29f66ff413cf191349bc593
hillary-wangui <hillary.maina@student.moringaschool.com>
2024-01-08 20:08:50 +0300

    assignment

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,20 @@
 #!/usr/bin/env python3
 
+num_list = [0, 1, 3, 5, 7, 8, 9]
+
 def return_evens(num_list):
-    pass
+    evens_list = [x for x in num_list if x % 2 == 0]
+    
+    return evens_list
+    
+print(return_evens(num_list))
+
+
+sentence_list = ["Hello", "I'm doing great", "Python is fun"]
 
 def make_exclamation(sentence_list):
-    pass
+    exclamation_list = [x + "!" for x in sentence_list ]
+
+    return exclamation_list
+    
+print(make_exclamation(sentence_list))
